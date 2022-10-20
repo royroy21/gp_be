9bfdc17ea6ee38bff8b62b4ad8ab78a791c396c4
Roy Hanley <royhanley8@gmail.com>
2022-10-20 16:30:15 +0100

    Added config for Heroku deployment

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Django backend for GP
+
+## Heroku
+This app uses Heroku for hosting: https://dashboard.heroku.com/apps
+
+Heroku CLI is used to deploy and configure apps: https://devcenter.heroku.com/articles/heroku-cli
+
+Handy Heroku commands:
+- To switch to a Heroku app: `heroku git:remote -a gigpig-backend`
+- To add an environmental variable: `heroku config:set SECRET_KEY=secretkey`
+- To view environmental variables: `heroku config`
--- project/settings/base.py
+++ project/settings/base.py
@@ -42,6 +42,7 @@ THIRD_PARTY_APPS = [
     "django_extensions",
     "rest_framework",
     "rest_framework_simplejwt",
+    "whitenoise.runserver_nostatic",
 ]
 
 INSTALLED_APPS = (
@@ -60,6 +61,7 @@ INSTALLED_APPS = (
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -114,7 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = "/static/"
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
