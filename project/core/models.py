316fc5803d1243682e3886f05a3c4e28fa0d6561
Roy Hanley <royhanley8@gmail.com>
2023-02-08 14:43:45 +0000

    Added active field to abstract BaseModel

--- project/core/models.py
+++ project/core/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class BaseModel(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
 
     class Meta:
         abstract = True
