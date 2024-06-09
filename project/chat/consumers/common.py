018e86fd817454bbeaa2668732030a5de988f388
roy <royhanley8@gmail.com>
2024-06-09 16:27:14 +0100

    Small refactor for logging errors @chat consumers.

--- project/chat/consumers/common.py
+++ project/chat/consumers/common.py
@@ -8,9 +8,10 @@ def format_user(user):
 
 
 def log_error(scope, message):
-    if scope["user"].is_authenticated:
-        user = format_user(scope["user"])
-    else:
-        user = "not authenticated"
+    user = (
+        format_user(scope["user"])
+        if scope["user"].is_authenticated
+        else "not authenticated"
+    )
     url_data = f"path:{scope['path']}, arguments:{scope['url_route']}"
     return logger.error("%s for user:%s, %s", message, user, url_data)
