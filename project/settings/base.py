dd38c8ed7d6e3badd224b85b1adec22ca0b01be4
roy <royhanley8@gmail.com>
2024-08-13 09:40:39 +0100

    Change access/refresh token lifetimes.

--- project/settings/base.py
+++ project/settings/base.py
@@ -199,8 +199,8 @@ REST_FRAMEWORK = {
 # DRF simple JWT
 # https://django-rest-framework-simplejwt.readthedocs.io/en/latest/settings.html
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "AUTH_HEADER_TYPES": ("JWT",),
