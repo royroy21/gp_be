7b45fd4550ec2d12018b040c946699b566d80962
roy <royhanley8@gmail.com>
2024-08-23 13:55:56 +0100

    Change to build_absolute_uri. If relative_uri starts with 'http' return relative_uri.

--- project/site/domain.py
+++ project/site/domain.py
@@ -7,5 +7,9 @@ def build_absolute_uri(relative_uri):
     Uses Site framework to provide absolute URL
     for when request object is not available.
     """
+    if relative_uri.startswith("http"):
+        # Relative URL already has full domain
+        return relative_uri
+
     domain = Site.objects.get_current().domain
     return f"{settings.SITE_SCHEME}://{domain}{relative_uri}"
